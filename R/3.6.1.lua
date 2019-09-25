db6a857c5a792c7913e5db38890650584213fc57
Dane Kennedy <dane@idia.ac.za>
2019-09-25 15:23:37 +0200

    DRY

--- R/3.6.1.lua
+++ R/3.6.1.lua
@@ -10,5 +10,12 @@ local singularity_image = "/cbio/images/bionic-R3.6.1-RStudio1.2.1335-bio.simg"
 
 setenv ("R_INSTALL_STAGED" ,"false")
 
-set_alias("R", "singularity run --app R " .. singularity_image )
-set_alias("Rscript", "singularity run --app Rscript " .. singularity_image )
+apps = {
+  "R",
+  "Rscript",
+  "rserver",
+}
+
+for i, app in ipairs(apps) do
+  set_alias(app, "singularity run --app " .. app .. " " .. singularity_image)
+end
